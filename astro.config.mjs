555827b08566dd1e4bcacc92732c53ad06467826
Ricardo Peralta <ricardoperaltag@gmail.com>
2025-09-10 20:29:33 +0000

    modificacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,31 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import astroIcon from 'astro-icon';
-import mdx from '@astrojs/mdx';
-import playformCompress from "@playform/compress";
-
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import astroIcon from "astro-icon";
+// import playformCompress from "@playform/compress"; // solo si lo instalaste
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config 
 export default defineConfig({
-  site: 'https://ricardoperalta.github.io',
-  integrations: [tailwind(), mdx(), astroIcon({
-    include: { 
-      mdi: ["*"], 
-      ri: ['*'],
-      'simple-icons': ['*'],
-    },
-  }), playformCompress({
-    CSS: false,
-    Image: false,
-    Action: {
-      Passed: async () => true,  
-    },
-  }), react()],
-  output: "server",
-  adapter: cloudflare(),
-  
+  site: "https://ricardoperalta.github.io",
+  integrations: [
+    tailwind(),
+    mdx(),
+    astroIcon({
+      include: {
+        mdi: ["*"],
+        ri: ["*"],
+        "simple-icons": ["*"],
+      },
+    }),
+    // playformCompress({
+    //   CSS: false,
+    //   Image: false,
+    //   Action: {
+    //     Passed: async () => true,
+    //   },
+    // }),
+    react(),
+  ],
+  output: "static", // GitHub Pages requiere archivos estáticos
 });
